088027a6aed91e57dbf4ce4da3302d6cb3d1398b
Jared Harbison <jared.harbison@gmail.com>
2018-12-20 22:30:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif  "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == .upcase
+    "NO, NOT SINCE 1938!"
+  elsif phrase == .upcase
+    "NO, NOT SINCE 1938!"
+  else  
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
