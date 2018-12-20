8104be218a31960a6915f8b8f22f79c0e243be08
Jared Harbison <jared.harbison@gmail.com>
2018-12-20 22:33:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma(phrase)
-phrase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
 end
